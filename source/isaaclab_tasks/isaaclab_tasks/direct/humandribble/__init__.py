a6edfc1bb7dca2780ba5db7ff6517fdc9742d4bb
leoriegel <leo.riegel@onlinehome.de>
2025-11-25 16:31:00 +0100

    Added Humandribble
    Changed imports
    Added math_utils to cfg, changed quat_rotate to quat_apply
    added self written normalize_angle version
    commented out the SAC cfg

---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/humandribble/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Humanoid dribbling environment.
+"""
+
+import gymnasium as gym
+
+from . import agents
+from .humandribble_env import HumandribbleEnv, HumandribbleEnvCfg
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-Humandribble-Direct-v0",
+    entry_point="isaaclab_tasks.direct.humandribble:HumandribbleEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": HumandribbleEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumandribblePPORunnerCfg",
+        # "rsl_rl_sac_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumandribbleSACRunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/humandribble/agents/rsl_rl_ppo_cfg.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab.utils import configclass
+
+from isaaclab_rl.rsl_rl import (
+    RslRlOnPolicyRunnerCfg,
+    RslRlPpoActorCriticCfg,
+    RslRlPpoAlgorithmCfg,
+    # RslRlOffPolicyRunnerCfg,
+    # RslRlSacActorCriticCfg,
+    # RslRlSacAlgorithmCfg,
+)
+
+
+@configclass
+class HumandribblePPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    rewards_expect = {
+        "rew_ball_vel": 2.0,
+    }
+    gage_init_std = 0.0
+    num_steps_per_env = 32
+    max_iterations = 500 # 12000
+    save_interval = 100 # 1000
+    experiment_name = "humandribble_direct"
+    empirical_normalization = True
+    policy = RslRlPpoActorCriticCfg(
+        class_name="ActorCritic",
+        init_noise_std=1.0,
+        actor_hidden_dims=[400, 200, 100],
+        critic_hidden_dims=[400, 200, 100],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.0,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-4,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.008,
+        max_grad_norm=1.0,
+    )
+
+# @configclass
+# class HumandribbleSACRunnerCfg(RslRlOffPolicyRunnerCfg):
+#     rewards_expect = {
+#         "rew_ball_vel": 2.0,
+#     }
+#     num_steps_per_env = 32
+#     max_iterations = 10000
+#     save_interval = 50
+#     experiment_name = "humandribble_direct"
+#     capacity_per_env = 300
+#     policy = RslRlSacActorCriticCfg(
+#         actor_hidden_dims=[400, 200, 100],
+#         critic_hidden_dims=[400, 200, 100],
+#         activation="elu",
+#         log_std_min=-5.0,
+#         log_std_max=5.0,
+#         use_layer_norm=False,
+#     )
+#     algorithm = RslRlSacAlgorithmCfg(
+#         alpha=1.0,
+#         num_learning_epochs=5,
+#         critic_lr=5.0e-4,
+#         actor_lr=5.0e-4,
+#         alpha_lr=5.0e-4,
+#         gamma=0.99,
+#         batch_size=8192,
+#         max_grad_norm=1.0,
+#         target_entropy=-10.0,
+#         tau=0.005,
+#         empirical_normalization=True,
+#     )
